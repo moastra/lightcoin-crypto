36e7301fcb153c8e8933e9280c4202487da70962
Mathew M <mathewmastromonaco@gmail.com>
2024-07-06 15:48:02 -0400

    implemented account creation using classes

--- index.js
+++ index.js
@@ -1,29 +1,82 @@
-let balance = 500.00;
+class Account {
 
-class Withdrawal {
-
-  constructor(amount) {
-    this.amount = amount;
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
   }
 
-  commit() {
-    balance -= this.amount;
+  get balance() {
+    let balance = 0;
+    for (const transaction of this.transactions) {
+      balance += transaction.value;
+    }
+    return balance;
   }
 
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
 }
 
+class Transaction {
 
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
+  }
+
+    commit() {
+      if (this.isAllowed()) {
+        this.time = new Date();
+        this.account.addTransaction(this);
+        return true;
+      } else {
+        return false;
+      }
+
+  }
+
+  isAllowed() {
+    return true;
+  }
+}
+  
+class Withdrawal extends Transaction {
+  get value() {
+    return -this.amount;
+  }
+
+  isAllowed() {
+    return this.account.balance >= this.amount;
+  }
+}
+
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount;
+  }
+
+  isAllowed() {
+    return true;
+  }
+}
 
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
-t1.commit();
-console.log('Transaction 1:', t1);
+const myAccount = new Account('snow-patrol');
 
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
+console.log('Starting Balance:', myAccount.balance);
 
-console.log('Balance:', balance);
+const t1 = new Deposit(120.00, myAccount);
+console.log('Transaction 1 Success:', t1.commit());
+
+const t2 = new Withdrawal(50.00, myAccount);
+console.log('Transaction 2 Success:', t2.commit());
+
+const t3 = new Withdrawal(200.00, myAccount);
+console.log('Transaction 3 Succes:', t3.commit());
+
+console.log('Ending Balance:', myAccount.balance);
